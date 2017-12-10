14c64300ee0921904097d2dcd5dd8374332339c9
jramaswami <jramaswami@gmail.com>
2017-12-10 13:50:11 -0500

    Solution to day 10.

--- 10/python/problem10.py
+++ 10/python/problem10.py
@@ -2,8 +2,10 @@
 Advent of Code 2017 :: Day 10
 Knot Hash
 """
+from operator import xor
+from functools import reduce
 
-INPUT = [189, 1, 111, 246, 254, 2, 0, 120, 215, 93, 255, 50, 84, 15, 94, 62]
+INPUT = "189,1,111,246,254,2,0,120,215,93,255,50,84,15,94,62"
 
 
 def reverse_slice(slice_start, slice_end, string_circle):
@@ -20,26 +22,64 @@ def reverse_slice(slice_start, slice_end, string_circle):
         string_circle[slice_start:slice_end] = slice_val[::-1]
 
 
-def knot_hash(twist_lengths, string_circle):
+def knot_hash(twist_lengths, string_circle, curr_pos=0, skip_size=0):
     """Returns knot hash."""
-    skip_size = 0
-    curr_pos = 0
     for twist_length in twist_lengths:
         reverse_slice(curr_pos, curr_pos+twist_length, string_circle)
         curr_pos = (curr_pos + twist_length + skip_size) % len(string_circle)
         skip_size += 1
+    return curr_pos, skip_size
 
 
 def solve_a():
     """Solves first part of puzzle."""
     A = list(range(256))
-    knot_hash(INPUT, A)
+    T = [int(i) for i in INPUT.split(',')]
+    knot_hash(T, A)
     return A[0] * A[1]
+
+
+def input_to_lengths(input_string):
+    """Converts input string to lengths."""
+    T = [ord(c) for c in input_string]
+    T.extend([17, 31, 73, 47, 23])
+    return T
+
+def knot_hash_loop(twist_lengths, string_circle):
+    """Knot hash 64 times."""
+    curr_pos = skip_size = 0
+    for _ in range(64):
+        curr_pos, skip_size = knot_hash(twist_lengths, string_circle, curr_pos, skip_size)
+
+
+def condense_hash(string_circle):
+    """Condenses into a dense hash."""
+    return [reduce(xor, string_circle[start:start+16]) for start in range(0, 256, 16)]
+
+
+def hash_to_string(string_circle):
+    """Convert hash to a string."""
+    return "".join([format(i, '02x') for i in string_circle])
+
+
+def complete_knot_hash(input_string):
+    """Returns completed knot hash."""
+    twist_lengths = input_to_lengths(input_string)
+    string_circle = list(range(256))
+    knot_hash_loop(twist_lengths, string_circle)
+    dense_hash = condense_hash(string_circle)
+    return hash_to_string(dense_hash)
+
+
+def solve_b():
+    """Solves second part of puzzle."""
+    return complete_knot_hash(INPUT)
 
 
 def main():
     """Main program."""
     print('The solution to part A is', solve_a())
+    print('The solution to part B is', solve_b())
 
 
 if __name__ == '__main__':
--- 10/python/test_problem10.py
+++ 10/python/test_problem10.py
@@ -32,3 +32,19 @@ def test_knot_hash():
     p10.knot_hash(T, A)
     assert A == [3, 4, 2, 1, 0]
 
+def test_solution_A():
+    """Test solution for part A."""
+    assert p10.solve_a() == 38415
+
+
+def test_input_to_lengths():
+    """Tests input_to_lengths()"""
+    assert p10.input_to_lengths('1,2,3') == [49, 44, 50, 44, 51, 17, 31, 73, 47, 23]
+
+
+def test_complete_knot_hash():
+    """Tests for complete_knot_hash()"""
+    assert p10.complete_knot_hash('') == 'a2582a3a0e66e6e86e3812dcb672a272'
+    assert p10.complete_knot_hash('AoC 2017') == '33efeb34ea91902bb2f59c9920caa6cd'
+    assert p10.complete_knot_hash('1,2,3') == '3efbe78a8d82f29979031a4aa0b16a9d'
+    assert p10.complete_knot_hash('1,2,4') == '63960835bcdc130f0b66d7ff4f6a5a8e'
