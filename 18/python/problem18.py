f5f8c1ef96626586e16fd32dbaf7d7a3591b5d11
jramaswami <jramaswami@gmail.com>
2017-12-18 16:47:32 -0500

    Improved solution. Used inheritance to clean things up.

--- 18/python/problem18.py
+++ 18/python/problem18.py
@@ -2,17 +2,12 @@
 Advent of Code 2017 :: Day 18
 Duet
 """
-import pyperclip
+
 from collections import defaultdict, deque
+import pyperclip
 
 
 LOGGING = False
-
-
-def log(*msg):
-    """Log msg to console."""
-    if LOGGING:
-        print(*msg)
 
 
 class DuetRunner():
@@ -20,8 +15,8 @@ class DuetRunner():
     def __init__(self):
         self.rcv_queue0 = deque()
         self.rcv_queue1 = deque()
-        self.machine0 = DuetMachine(DuetMachine.B, 0, self.rcv_queue0, self.rcv_queue1)
-        self.machine1 = DuetMachine(DuetMachine.B, 1, self.rcv_queue1, self.rcv_queue0)
+        self.machine0 = DuetMachineB(0, self.rcv_queue0, self.rcv_queue1)
+        self.machine1 = DuetMachineB(1, self.rcv_queue1, self.rcv_queue0)
     
     def load_program(self, program):
         """Load program into both machines."""
@@ -30,74 +25,54 @@ class DuetRunner():
 
     def run(self):
         """Run both machines until that deadlock or halt."""
-        while not self.machine0.deadlock and not self.machine1.deadlock \
-              and not self.machine0.halt and not self.machine1.halt:
-            self.machine0.runB()
-            self.machine1.runB()
-    
+        while self.machine0.runnable() and self.machine1.runnable():
+            self.machine0.run()
+            self.machine1.run()
+            self.machine0.unblock()
+            self.machine1.unblock()
 
-class DuetMachine:
+
+class BaseDuetMachine:
     """Represents the computer to run the duet program."""
-    
-    A = 0
-    B = 1
-
-    def __init__(self, machine_type=A, machine_id=0, rcv_q=None, snd_q=None):
+    def __init__(self):
         self.instruction_ptr = 0
         self.instructions = []
         self.registers = defaultdict(int)
-        self.freq = 0
-        self.prev_freq = 0
-        self.first_rcv = 0
-        self.rcv_count = 0
-        self.snd_count = 0
-        self.deadlock = False
-        self.blocking = False
         self.halt = False
-        self.id = machine_id
-        if machine_type == DuetMachine.A:
-            self.snd = self.sndA
-            self.rcv = self.rcvA
-        else:
-            self.snd = self.sndB
-            self.rcv = self.rcvB
-            self.snd_q = snd_q
-            self.rcv_q = rcv_q
-            self.registers['p'] = machine_id
-    
+
+    def log(self, *msg):
+        """Log msg to console."""
+        if LOGGING:
+            print(*msg)
+
     def load_program(self, program):
         """Load program from string."""
         self.instructions = [line.strip().split() for line in program.split('\n')]
-
-    def runA(self):
-        """Run program until first working recvA"""
-        log('Running ...')
         self.instruction_ptr = 0
-        while self.instruction_ptr >= 0 \
-        and self.instruction_ptr < len(self.instructions) \
-        and not self.first_rcv:
-            self.tick()
-    
-    def runB(self):
-        """Run program until blocked."""
-        log('Running {}'.format(self.id))
-        self.blocking = False
-        while not self.halt and not self.deadlock and not self.blocking:
+
+    def runnable(self):
+        """Returns True if program is runnable."""
+        return not self.halt
+
+    def run(self):
+        """Run program."""
+        self.log('Running ...')
+        while self.runnable():
             self.tick()
 
     def tick(self):
         """Complete on instruction."""
-        if self.halt:
+        if not self.runnable():
             return
         tokens = self.instructions[self.instruction_ptr]
-        log(self.instruction_ptr, ':', tokens)
+        self.log(self.instruction_ptr, ':', tokens)
         func = getattr(self, tokens[0])
         if len(tokens) == 3:
             func(tokens[1], tokens[2])
         elif len(tokens) == 2:
             func(tokens[1])
         else:
-            log("ERROR", tokens)
+            self.log("ERROR", tokens)
         if self.instruction_ptr < 0 or self.instruction_ptr >= len(self.instructions):
             self.halt = True
 
@@ -112,53 +87,10 @@ class DuetMachine:
         else:
             return self.registers[arg]
 
-    def sndA(self, arg):
-        """snd instruction for machine a"""
-        self.freq = self.get_value(arg)
-        log("sndA({}={})".format(arg, self.freq))
-        self.instruction_ptr += 1
-    
-    def sndB(self, arg):
-        """snd instruction for machine b"""
-        val = self.get_value(arg)
-        log("{}: sndB({})".format(self.id, val))
-        self.snd_count += 1
-        self.snd_q.append(val)
-        self.instruction_ptr += 1
-
-    def rcvA(self, arg):
-        """rcv instruction for machine a"""
-        val = self.get_value(arg)
-        log("{}: rcvA({}={})".format(self.id, arg, val))
-        if val != 0:
-            self.prev_freq = self.freq
-            self.freq = 0
-            if not self.first_rcv:
-                log('first rcv')
-                self.first_rcv = self.prev_freq
-        self.instruction_ptr += 1
-    
-    def rcvB(self, reg):
-        """rcv instruction for machine b"""
-        if self.rcv_q:
-            self.rcv_count = 0
-            self.registers[reg] = self.rcv_q.popleft()
-            log("{}: rcvB({}) {} ok".format(self.id, reg, self.registers[reg]))
-            self.instruction_ptr += 1
-        else:
-            self.rcv_count = self.rcv_count + 1
-            if self.rcv_count == 4:
-                self.deadlock = True
-                log("{}: rcvB({}) DEADLOCK {}".format(self.id, reg, self.rcv_count))
-            else:
-                self.blocking = True
-                log("{}: rcvB({}) FAILED {}".format(self.id, reg, self.rcv_count))
-
-
     def set(self, reg, arg):
         """set instruction"""
         val = self.get_value(arg)
-        log("{}: set({}, {}={})".format(self.id, reg, arg, val))
+        self.log("set({}, {}={})".format(reg, arg, val))
         self.registers[reg] = val
         self.instruction_ptr += 1
     
@@ -166,7 +98,7 @@ class DuetMachine:
         """add instruction"""
         val1 = self.get_value(reg)
         val2 = self.get_value(arg)
-        log("{}: add({}={}, {}={})".format(self.id, reg, val1, arg, val2))
+        self.log("add({}={}, {}={})".format(reg, val1, arg, val2))
         self.registers[reg] = val1 + val2
         self.instruction_ptr += 1
 
@@ -174,7 +106,7 @@ class DuetMachine:
         """mul instruction"""
         val1 = self.get_value(reg)
         val2 = self.get_value(arg)
-        log("{}: mul({}={}, {}={})".format(self.id, reg, val1, arg, val2))
+        self.log("mul({}={}, {}={})".format(reg, val1, arg, val2))
         self.registers[reg] = val1 * val2
         self.instruction_ptr += 1
 
@@ -182,7 +114,7 @@ class DuetMachine:
         """mod instruction"""
         val1 = self.get_value(reg)
         val2 = self.get_value(arg)
-        log("{}: mod({}={}, {}={})".format(self.id, reg, val1, arg, val2))
+        self.log("mod({}={}, {}={})".format(reg, val1, arg, val2))
         self.registers[reg] = val1 % val2
         self.instruction_ptr += 1
 
@@ -190,25 +122,104 @@ class DuetMachine:
         """jgz instruction"""
         val1 = self.get_value(arg1)
         off = self.get_value(arg2)
-        log("{}: jgz({}={}, {}={})".format(self.id, arg1, val1, arg2, off))
+        self.log("jgz({}={}, {}={})".format(arg1, val1, arg2, off))
         if val1 > 0:
             self.instruction_ptr += off
         else:
             self.instruction_ptr += 1
 
+class DuetMachineA(BaseDuetMachine):
+    """Represents the computer for first part of puzzle."""
+
+    def __init__(self):
+        super(DuetMachineA, self).__init__()
+        self.freq = 0
+        self.prev_freq = 0
+        self.first_rcv = 0
+
+    def snd(self, arg):
+        """snd instruction for machine a"""
+        self.freq = self.get_value(arg)
+        self.log("snd({}={})".format(arg, self.freq))
+        self.instruction_ptr += 1
+
+    def rcv(self, arg):
+        """rcv instruction for machine a"""
+        val = self.get_value(arg)
+        self.log("rcv({}={})".format(arg, val))
+        if val != 0:
+            self.prev_freq = self.freq
+            self.freq = 0
+            if not self.first_rcv:
+                self.log('first rcv')
+                self.first_rcv = self.prev_freq
+                self.halt = True
+        self.instruction_ptr += 1
+
+
+class DuetMachineB(BaseDuetMachine):
+    """Represents computer for part B of puzzle."""
+    def __init__(self, machine_id, rcv_q, snd_q):
+        super(DuetMachineB, self).__init__()
+        self.rcv_count = 0
+        self.snd_count = 0
+        self.deadlock = False
+        self.blocking = False
+        self.id = machine_id
+        self.snd_q = snd_q
+        self.rcv_q = rcv_q
+        self.registers['p'] = machine_id
+
+    def log(self, *msg):
+        """Log message to console."""
+        super(DuetMachineB, self).log(self.id, ":", *msg)
+
+    def unblock(self):
+        self.blocking = False
+
+    def runnable(self):
+        """Returns True if program is runnable."""
+        return not self.blocking and not self.deadlock and super(DuetMachineB, self).runnable()
+
+    def snd(self, arg):
+        """snd instruction for machine b"""
+        val = self.get_value(arg)
+        self.log("snd({})".format(val))
+        self.snd_count += 1
+        self.snd_q.append(val)
+        self.instruction_ptr += 1
+    
+    def rcv(self, reg):
+        """rcv instruction for machine b"""
+        if self.rcv_q:
+            self.rcv_count = 0
+            self.registers[reg] = self.rcv_q.popleft()
+            self.log("rcv({}) {} ok".format(reg, self.registers[reg]))
+            self.instruction_ptr += 1
+        else:
+            self.rcv_count = self.rcv_count + 1
+            if self.rcv_count == 4:
+                self.deadlock = True
+                self.log("rcvB({}) DEADLOCK {}".format(reg, self.rcv_count))
+            else:
+                self.blocking = True
+                self.log("rcvB({}) FAILED {}".format(reg, self.rcv_count))
+
+
 def main():
     """Main program."""
     import sys
     program = sys.stdin.read()
-    machine = DuetMachine()
+    machine = DuetMachineA()
     machine.load_program(program)
-    machine.runA()
+    machine.run()
     print('Solution to part A is', machine.first_rcv)
     duet = DuetRunner()
     duet.load_program(program)
     duet.run()
     print('Solution to part B is', duet.machine1.snd_count)
     pyperclip.copy(str(duet.machine1.snd_count))
+
 
 if __name__ == '__main__':
     main()
--- 18/python/test_problem18.py
+++ 18/python/test_problem18.py
@@ -26,7 +26,7 @@ rcv d"""
 
 def test_machine_set():
     """Tests Duet Machine set()"""
-    machine = p18.DuetMachine()
+    machine = p18.DuetMachineA()
     machine.set('a', '1')
     assert machine.registers['a'] == 1
     assert machine.instruction_ptr == 1
@@ -35,18 +35,18 @@ def test_machine_set():
     assert machine.instruction_ptr == 2
 
 def test_machine_sndA():
-    """Tests Duet Machine sndA()"""
-    machine = p18.DuetMachine()
+    """Tests Duet Machine A snd()"""
+    machine = p18.DuetMachineA()
     machine.set('a', '1')
-    machine.sndA('a')
+    machine.snd('a')
     assert machine.freq == 1
     assert machine.instruction_ptr == 2
 
 def test_machine_rcvA():
-    """Tests Duet Machine sndA()"""
-    machine = p18.DuetMachine()
+    """Tests Duet Machine A rcv()"""
+    machine = p18.DuetMachineA()
     machine.set('a', '1')
-    machine.sndA('a')
+    machine.snd('a')
     machine.rcv('b')
     assert machine.freq == 1
     assert machine.instruction_ptr == 3
@@ -55,7 +55,7 @@ def test_machine_rcvA():
     assert machine.prev_freq == 1
     assert machine.freq == 0
     assert machine.instruction_ptr == 5
-    machine.sndA('b')
+    machine.snd('b')
     machine.rcv('0')
     assert machine.prev_freq == 1
     assert machine.freq == 4
@@ -67,7 +67,7 @@ def test_machine_rcvA():
 
 def test_machine_add():
     """Test DuetMachine add()"""
-    machine = p18.DuetMachine()
+    machine = p18.DuetMachineA()
     machine.set('a', '1')
     machine.add('a', '2')
     assert machine.registers['a'] == 3
@@ -78,7 +78,7 @@ def test_machine_add():
 
 def test_machine_mul():
     """Test DuetMachine mul()"""
-    machine = p18.DuetMachine()
+    machine = p18.DuetMachineA()
     machine.set('b', '2')
     machine.mul('b', '2')
     assert machine.registers['b'] == 4
@@ -90,7 +90,7 @@ def test_machine_mul():
 
 def test_machine_mod():
     """Test DuetMachine mod()"""
-    machine = p18.DuetMachine()
+    machine = p18.DuetMachineA()
     machine.set('b', '5')
     machine.mod('b', '2')
     assert machine.registers['b'] == 1
@@ -103,7 +103,7 @@ def test_machine_mod():
 
 def test_machine_jgz():
     """Test DuetMachine jgz()"""
-    machine = p18.DuetMachine()
+    machine = p18.DuetMachineA()
     machine.set('a', '2')
     machine.jgz('b', 'a')
     assert machine.instruction_ptr == 2
@@ -118,9 +118,9 @@ def test_machine_jgz():
 
 def test_machine_run():
     """Tests Duet Machine run()"""
-    machine = p18.DuetMachine()
+    machine = p18.DuetMachineA()
     machine.load_program(TEST_PROGRAM_A)
-    machine.runA()
+    machine.run()
     assert machine.first_rcv == 4
 
 def test_duet_run():
@@ -128,4 +128,4 @@ def test_duet_run():
     duet = p18.DuetRunner()
     duet.load_program(TEST_PROGRAM_B)
     duet.run()
-    assert duet.machine1.send_count == 3
+    assert duet.machine1.snd_count == 3
