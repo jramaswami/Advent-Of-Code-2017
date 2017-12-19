3a95f845d5cf2b2724c5f7cc61734d44f6af4464
jramaswami <jramaswami@gmail.com>
2017-12-19 09:49:51 -0500

    Solution to day 19 part A.

--- 19/python/problem19.py
+++ 19/python/problem19.py
@@ -3,54 +3,57 @@ Advent of Code 2017 :: Day 19
 A Series of Tubes
 """
 from collections import namedtuple
+import pyperclip
 
-Pos = namedtuple(Pos, ['row', 'col'])
-Pos.__add__ = lambda s, o: Pos(s.row + o.row, s.col + o.col)
 
-def get_neighbors_4(row, col, height, width):
+Position = namedtuple('Position', ['row', 'col'])
+Position.__add__ = lambda s, o: Position(s.row + o.row, s.col + o.col)
+Position.__sub__ = lambda s, o: Position(s.row - o.row, s.col - o.col)
+
+def get_neighbors_4(pos, maze):
     "Gets the neighbors of given cell: right, left, down, up."
-    if col + 1 < width:
-        yield (row, col + 1)
-    if col - 1 >= 0:
-        yield (row, col - 1)
-    if row + 1 < height:
-        yield (row + 1, col)
-    if row - 1 >= 0:
-        yield (row - 1, col)
+    directions = [Position(0, -1), Position(0, 1), Position(-1, 0), Position(1, 0)]
+    return [pos + dirn for dirn in directions if inbounds(pos + dirn, maze)]
 
 
 def find_start(maze):
     """Find the start point of maze"""
     col = maze[0].index('|')
-    return (0, col)
+    return Position(0, col)
 
 
-def next_move(position, maze, visited):
-    row, col = position
-    char = maze[row][col]
-    next_pos = None
-    if char == '|':
-        if row > 0 and not visited[row-1][col]:
-            next_pos = (row - 1, col)
-        else:
-            next_pos = (row + 1, col)
-    elif char == '-':
-        if not visited[row][col-1]:
-            next_pos = (row, col - 1)
-        else:
-            next_pos = (row, col + 1)
+def inbounds(pos, maze):
+    """Returns true if position is inside maze."""
+    width = len(maze[0])
+    height = len(maze)
+    return pos.row >= 0 and pos.row < height and pos.col >= 0 and pos.col < width
+
+
+def maze_char(pos, maze):
+    """Returns the char at that position."""
+    return maze[pos.row][pos.col]
+
+
+def visit(pos, visited):
+    """Sets visited to true for given pos."""
+    visited[pos.row][pos.col] = True
+
+
+def is_visited(pos, visited):
+    """Returns if position has been visited."""
+    return visited[pos.row][pos.col]
+
+
+def next_move(pos, dirn, maze, visited):
+    """Get the next move."""
+    next_pos = pos + dirn
+    if inbounds(next_pos, maze) and maze_char(next_pos, maze) != ' ':
+        return (next_pos, dirn)
     else:
-        width = len(maze[0])
-        height = len(maze)
-        for row_v, col_v in get_neighbors_4(row, col, height, width):
-            if maze[row_v][col_v] != ' ' and not visited[row_v][col_v]:
-                next_pos = (row_v, col_v) 
-    
-    if not next_pos or maze[next_pos[0]][next_pos[1]] == ' ':
-        return (-1, -1)
-    else:
-        return next_pos
-
+        for next_pos in get_neighbors_4(pos, maze):
+            if maze_char(next_pos, maze) != ' ' and not is_visited(next_pos, visited):
+                return (next_pos, next_pos - pos)
+    return (None, None)
 
 def solve_a(maze):
     """Solve first part of puzzle."""
@@ -58,26 +61,23 @@ def solve_a(maze):
     width = len(maze[0])
     height = len(maze)
     visited = [[False for _ in range(width)] for _ in range(height)]
-    row, col = find_start(maze)
-    visited[row][col] = True
-    direction = (-1, 0)
-    while True:
-    while row >= 0 and col >= 0:
-        print(row, col, maze[row][col])
-        row, col = next_move((row, col), maze, visited)
-        visited[row][col] = True
-        if maze[row][col].isalpha():
-            message.append(maze[row][col])
+    pos = find_start(maze)
+    dirn = Position(1, 0)
+    while pos:
+        visit(pos, visited)
+        if maze_char(pos, maze).isalpha():
+            message.append(maze_char(pos, maze))
+        pos, dirn = next_move(pos, dirn, maze, visited)
     return ''.join(message)
 
 
 def main():
     """Main program."""
     import sys
-    maze = [line[:-1] for line in sys.stdin]
-    for row in maze:
-        print(row)
-    print('The solution to Part A is', solve_a(maze))
+    maze = [line[:-1] for line in sys.stdin if line.strip()]
+    solution_a = solve_a(maze)
+    print('The solution to Part A is', solution_a)
+    pyperclip.copy(solution_a)
 
 
 if __name__ == '__main__':
