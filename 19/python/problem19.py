cc2cb3577be1680a990e0c187687444f3f17696e
jramaswami <jramaswami@gmail.com>
2017-12-19 10:12:18 -0500

    Solution to day 19.

--- 19/python/problem19.py
+++ 19/python/problem19.py
@@ -10,74 +10,81 @@ Position = namedtuple('Position', ['row', 'col'])
 Position.__add__ = lambda s, o: Position(s.row + o.row, s.col + o.col)
 Position.__sub__ = lambda s, o: Position(s.row - o.row, s.col - o.col)
 
-def get_neighbors_4(pos, maze):
-    "Gets the neighbors of given cell: right, left, down, up."
-    directions = [Position(0, -1), Position(0, 1), Position(-1, 0), Position(1, 0)]
-    return [pos + dirn for dirn in directions if inbounds(pos + dirn, maze)]
+class Maze:
+    def __init__(self, input_string):
+        self.steps = 0
+        self.maze = [line for line in input_string.split('\n')]
+        height = len(self.maze)
+        width = len(self.maze[0])
+        self.visited = visited = [[False for _ in range(width)] for _ in range(height)]
+        self.message = []
 
+    def find_start(self):
+        """Find the start point of maze"""
+        col = self.maze[0].index('|')
+        return Position(0, col)
 
-def find_start(maze):
-    """Find the start point of maze"""
-    col = maze[0].index('|')
-    return Position(0, col)
+    def get_neighbors_4(self, pos):
+        "Gets the neighbors of given cell: right, left, down, up."
+        directions = [Position(0, -1), Position(0, 1), Position(-1, 0), Position(1, 0)]
+        return [pos + dirn for dirn in directions if self.inbounds(pos + dirn)]
 
+    def inbounds(self, pos):
+        """Returns true if position is inside maze."""
+        width = len(self.maze[0])
+        height = len(self.maze)
+        return pos.row >= 0 and pos.row < height and pos.col >= 0 and pos.col < width
 
-def inbounds(pos, maze):
-    """Returns true if position is inside maze."""
-    width = len(maze[0])
-    height = len(maze)
-    return pos.row >= 0 and pos.row < height and pos.col >= 0 and pos.col < width
+    def maze_char(self, pos):
+        """Returns the char at that position."""
+        return self.maze[pos.row][pos.col]
 
+    def visit(self, pos):
+        """Sets visited to true for given pos."""
+        self.visited[pos.row][pos.col] = True
 
-def maze_char(pos, maze):
-    """Returns the char at that position."""
-    return maze[pos.row][pos.col]
+    def is_visited(self, pos):
+        """Returns if position has been visited."""
+        return self.visited[pos.row][pos.col]
 
+    def next_move(self, pos, dirn):
+        """Get the next move."""
+        next_pos = pos + dirn
+        if self.inbounds(next_pos) and self.maze_char(next_pos) != ' ':
+            return (next_pos, dirn)
+        else:
+            for next_pos in self.get_neighbors_4(pos):
+                if self.maze_char(next_pos) != ' ' and not self.is_visited(next_pos):
+                    return (next_pos, next_pos - pos)
+        return (None, None)
 
-def visit(pos, visited):
-    """Sets visited to true for given pos."""
-    visited[pos.row][pos.col] = True
+    def run_maze(self):
+        """Run through the maze."""
+        pos = self.find_start()
+        dirn = Position(1, 0)
+        while pos:
+            self.visit(pos)
+            self.steps += 1
+            if self.maze_char(pos).isalpha():
+                self.message.append(self.maze_char(pos))
+            pos, dirn = self.next_move(pos, dirn)
 
+    def solution_a(self):
+        """Return the solution to part A."""
+        return ''.join(self.message)
 
-def is_visited(pos, visited):
-    """Returns if position has been visited."""
-    return visited[pos.row][pos.col]
-
-
-def next_move(pos, dirn, maze, visited):
-    """Get the next move."""
-    next_pos = pos + dirn
-    if inbounds(next_pos, maze) and maze_char(next_pos, maze) != ' ':
-        return (next_pos, dirn)
-    else:
-        for next_pos in get_neighbors_4(pos, maze):
-            if maze_char(next_pos, maze) != ' ' and not is_visited(next_pos, visited):
-                return (next_pos, next_pos - pos)
-    return (None, None)
-
-def solve_a(maze):
-    """Solve first part of puzzle."""
-    message = []
-    width = len(maze[0])
-    height = len(maze)
-    visited = [[False for _ in range(width)] for _ in range(height)]
-    pos = find_start(maze)
-    dirn = Position(1, 0)
-    while pos:
-        visit(pos, visited)
-        if maze_char(pos, maze).isalpha():
-            message.append(maze_char(pos, maze))
-        pos, dirn = next_move(pos, dirn, maze, visited)
-    return ''.join(message)
-
+    def solution_b(self):
+        """Return the solution to part B."""
+        return self.steps
 
 def main():
     """Main program."""
     import sys
-    maze = [line[:-1] for line in sys.stdin if line.strip()]
-    solution_a = solve_a(maze)
-    print('The solution to Part A is', solution_a)
-    pyperclip.copy(solution_a)
+    maze = Maze(sys.stdin.read())
+    maze.run_maze()
+    print('The solution to Part A is', maze.solution_a())
+    print('The solution to Part B is', maze.solution_b())
+    pyperclip.copy(str(maze.solution_b()))
 
 
 if __name__ == '__main__':
